d5a22ce2459bf55e5c241a7eb3f2d1d085a6c5cc
Yavor <adidas97yg@gmail.com>
2020-06-08 17:18:27 +0300

    init

--- SymbolTableProject/Form1.Designer.cs
+++ SymbolTableProject/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace SymbolTableProject
+﻿using System.Windows.Forms;
+
+namespace SymbolTableProject
 {
     partial class Form1
     {
@@ -28,13 +30,185 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.symbolTableLbl = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Pos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Symbol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Token = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.keyTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.valueTxt = new System.Windows.Forms.ComboBox();
+            this.indexTxt = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(22, 12);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(88, 34);
+            this.clearBtn.TabIndex = 0;
+            this.clearBtn.Text = "CLEAR";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // symbolTableLbl
+            // 
+            this.symbolTableLbl.AutoSize = true;
+            this.symbolTableLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.symbolTableLbl.Location = new System.Drawing.Point(156, 17);
+            this.symbolTableLbl.Name = "symbolTableLbl";
+            this.symbolTableLbl.Size = new System.Drawing.Size(175, 29);
+            this.symbolTableLbl.TabIndex = 1;
+            this.symbolTableLbl.Text = "Symbol Table";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToOrderColumns = true;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Pos,
+            this.Symbol,
+            this.Token});
+            this.dataGridView.Location = new System.Drawing.Point(12, 66);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(573, 415);
+            this.dataGridView.TabIndex = 2;
+            this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Pos
+            // 
+            this.Pos.HeaderText = "Pos";
+            this.Pos.Name = "Pos";
+            this.Pos.ReadOnly = true;
+            this.Pos.Width = 40;
+            // 
+            // Symbol
+            // 
+            this.Symbol.HeaderText = "Symbol (lexeme)";
+            this.Symbol.Name = "Symbol";
+            this.Symbol.ReadOnly = true;
+            this.Symbol.Width = 200;
+            // 
+            // Token
+            // 
+            this.Token.HeaderText = "Token (code)";
+            this.Token.Name = "Token";
+            this.Token.ReadOnly = true;
+            this.Token.Width = 130;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(604, 210);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(398, 92);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(763, 225);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(81, 27);
+            this.addBtn.TabIndex = 4;
+            this.addBtn.Text = "Symb";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(720, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "( \'";
+            // 
+            // keyTxt
+            // 
+            this.keyTxt.Location = new System.Drawing.Point(750, 269);
+            this.keyTxt.Name = "keyTxt";
+            this.keyTxt.Size = new System.Drawing.Size(45, 22);
+            this.keyTxt.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(801, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "\' ,";
+            // 
+            // valueTxt
+            // 
+            this.valueTxt.FormattingEnabled = true;
+            this.valueTxt.Items.AddRange(new object[] {
+            "Ident",
+            "Number",
+            "KeyWord",
+            "Oper"});
+            this.valueTxt.Location = new System.Drawing.Point(829, 267);
+            this.valueTxt.Name = "valueTxt";
+            this.valueTxt.Size = new System.Drawing.Size(58, 24);
+            this.valueTxt.TabIndex = 8;
+            // 
+            // indexTxt
+            // 
+            this.indexTxt.Location = new System.Drawing.Point(1008, 245);
+            this.indexTxt.Name = "indexTxt";
+            this.indexTxt.Size = new System.Drawing.Size(33, 22);
+            this.indexTxt.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1055, 492);
+            this.Controls.Add(this.indexTxt);
+            this.Controls.Add(this.valueTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.keyTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.symbolTableLbl);
+            this.Controls.Add(this.clearBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Label symbolTableLbl;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private DataGridViewTextBoxColumn Pos;
+        private DataGridViewTextBoxColumn Symbol;
+        private DataGridViewTextBoxColumn Token;
+        private PictureBox pictureBox1;
+        private Button addBtn;
+        private Label label1;
+        private TextBox keyTxt;
+        private Label label2;
+        private ComboBox valueTxt;
+        private TextBox indexTxt;
     }
 }
 
--- SymbolTableProject/Form1.cs
+++ SymbolTableProject/Form1.cs
@@ -12,9 +12,93 @@ namespace SymbolTableProject
 {
     public partial class Form1 : Form
     {
+        
+        Dictionary<string, string> tableData;
         public Form1()
         {
+            tableData = new Dictionary<string, string>();
+
             InitializeComponent();
+            fill();
+        }
+
+        
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+
+
+        }
+
+        private void fill ()
+        {
+            for (int i = 1; i <= 13; i++)
+            {
+                dataGridView.Rows.Add(i.ToString());
+
+            }
+        }
+
+       
+
+        
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            string key = keyTxt.Text;
+            string value = valueTxt.Text;
+            
+
+            if(!tableData.ContainsKey(key))
+            {
+                tableData.Add(key, value);
+                refresh();
+            }
+            else
+            {
+                var a = tableData.Keys.ToList().IndexOf(key);
+
+                a++;
+                indexTxt.Text = a.ToString();
+
+            }
+
+           
+
+
+        }
+
+        private void refresh()
+        {
+            int count = 0;
+            foreach (var item in tableData)
+            {
+                
+                dataGridView.Rows[count].Cells[1].Value = item.Key;
+                dataGridView.Rows[count].Cells[2].Value = item.Value;
+                count++;
+               
+            }
+            indexTxt.Text = count.ToString();
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            tableData = new Dictionary<string, string>();
+            refresh();
+            dataGridView.Rows.Clear();
+            fill();
+            keyTxt.Text = "";
+            valueTxt.Text = "";
+            indexTxt.Text = "";
         }
     }
 }
